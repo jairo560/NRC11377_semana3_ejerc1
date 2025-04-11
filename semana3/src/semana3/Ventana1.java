879bef52bb979a377f7819424edaaffd368e765a
UPN <UPN@ULCLAB1318>
2025-04-11 16:13:06 -0500

    segundo commit

--- semana3/src/semana3/Ventana1.java
+++ semana3/src/semana3/Ventana1.java
@@ -103,6 +103,7 @@ public class Ventana1 extends JFrame implements ActionListener {
 		contentPane.add(scrollPane);
 		
 		txtS = new JTextArea();
+		txtS.setText("Verificando");
 		scrollPane.setViewportView(txtS);
 	}
 	public void actionPerformed(ActionEvent e) {
